f4b39a4f5369c6f80a3455acf821fb4cf559bd56
Guille1684x <108602156+Guille1684x@users.noreply.github.com>
2023-10-24 09:09:33 -0600

    Update CV9.sql
    Agregué las claves primarias y foráneas a las tablas

--- CV9.sql
+++ CV9.sql
@@ -2,6 +2,8 @@ create database fillgaps;
 
 use fillgaps;
 
+-- Personal
+
 create table Personal (
 	ID int not null,
     Nombre varchar(20) not null,
@@ -14,11 +16,40 @@ create table Personal (
     ID_Item int not null
 );
 
+-- Llave primaria
+ALTER TABLE Personal
+ADD CONSTRAINT pk_Personal PRIMARY KEY (ID);
+
+-- Llaves foráneas
+ALTER TABLE Personal
+ADD CONSTRAINT fk_Personal_Rol
+FOREIGN KEY (ID Rol)
+REFERENCES Rol(ID);
+
+ALTER TABLE Personal
+ADD CONSTRAINT fk_Personal_Reportes
+FOREIGN KEY (ID Reporte)
+REFERENCES Reportes(ID);
+
+ALTER TABLE Personal
+ADD CONSTRAINT fk_Personal_Item 
+FOREIGN KEY (ID Item)
+REFERENCES Item(ID);
+
+-- Rol
+	
 create table Rol (
 	ID int not null,
     Rol varchar(20) not null
 );
 
+-- Llave primaria 
+
+ALTER TABLE Rol
+ADD CONSTRAINT pk_Rol PRIMARY KEY (ID);
+
+-- Reportes
+	
 create table Reportes (
 	ID int not null,
     Descripcion varchar(30) not null,
@@ -28,6 +59,17 @@ create table Reportes (
     ID_Item int not null
 );
 
+-- Llave primaria
+ALTER TABLE Reportes
+ADD CONSTRAINT pk_Reportes ADD PRIMARY KEY (ID);
+
+-- Llave foránea
+ALTER TABLE Reportes
+ADD CONSTRAINT fk_Reportes_Item
+FOREIGN KEY (ID item)
+REFERENCES Item(ID);
+
+-- Item
 create table Item (
 	ID int not null,
     Nombre varchar(20) not null,
@@ -37,10 +79,26 @@ create table Item (
     Campo varchar(20)
 );
 
+-- Llave primaria
+ALTER TABLE Item
+ADD CONSTRAINT pk_Item PRIMARY KEY (ID);
+
+-- Llave foránea
+ALTER TABLE Item
+ADD CONSTRAINT fk_Item_Personal
+FOREIGN KEY (ID Personal)
+REFERENCES Personal(ID);
+
+ALTER TABLE Item
+ADD CONSTRAINT fk_Item_Prestamo
+FOREIGN KEY (ID Prestamo)
+REFERENCES Prestamo(ID)
+
+/*
 alter table Personal
 add primary key (ID);
 
 alter table Rol
 add primary key (ID);
-
+*/
 desc Rol;
