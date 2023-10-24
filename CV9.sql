73f65578112f586d612d650cf9a05e207aae9903
Saúl Hernández <saulhdza.17@gmail.com>
2023-10-24 00:30:17 -0600

    Creación de la tabla "Personal"

--- CV9.sql
+++ CV9.sql
@@ -1,1 +1,16 @@
 create database fillgaps;
+
+use fillgaps;
+
+create table Personal (
+	ID int not null,
+    Nombre varchar(20) not null,
+    CURP varchar(18) not null,
+    RFC varchar(13) not null,
+    FechaContratacion date not null,
+    edad int not null,
+    ID_Rol int not null,
+    ID_Reporte int not null,
+    ID_Item int not null
+);
+
