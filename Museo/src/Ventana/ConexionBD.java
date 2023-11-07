a9f45faa2ff5a569ed9b040c50fde1c147f9ab7f
Saúl Hernández <saulhdza.17@gmail.com>
2023-11-07 02:37:37 -0600

    Conexión a la base de datos.
    Queda pendiente la dependencia de MySQL.

---
+++ Museo/src/Ventana/ConexionBD.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ventana;
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author SAUL
+ */
+public class ConexionBD {
+    Connection conectar = null;
+    String usuario = "root";
+    String contraseña = "Alonso.172001";
+    String bd = "fillgaps";
+    String ip = "localhost";
+    String puerto ="3306";
+    
+    String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + bd;
+    
+    public Connection establecerConexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //Queda pendiente la dependencia de SQL.
+            conectar = DriverManager.getConnection(cadena, usuario, contraseña);
+            JOptionPane.showMessageDialog(null, "Se conectó a la base de datos");
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(null, "No se conectó a la base de datos" + e.toString());
+        }
+        
+        return conectar;
+    }
+}
---
+++ Museo/src/Ventana/Inicio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ventana;
+
+/**
+ *
+ * @author SAUL
+ */
+public class Inicio {
+    public static void main(String[] args) {
+        Main objetoMain = new Main();
+        objetoMain.setVisible(true);
+    }
+}
