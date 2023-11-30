94ca803727129a044446cf94468584a8014accaf
Axel Jauregui <jauregui@iMac-de-Axel.local>
2023-11-29 19:24:52 -0600

    Agrego el Menu Lateral de los Conservadores y Restauradores

--- Museo/src/Ventana/MenuLateral_CR.java
+++ Museo/src/Ventana/MenuLateral_CR.java
@@ -11,12 +11,12 @@ import javax.swing.SwingUtilities;
  *
  * @author jauregui
  */
-public class MenuLateral_IC extends javax.swing.JPanel {
+public class MenuLateral_CR extends javax.swing.JPanel {
 
     /**
-     * Creates new form MenuLateral_ICE
+     * Creates new form MenuLateral_CR
      */
-    public MenuLateral_IC() {
+    public MenuLateral_CR() {
         initComponents();
     }
 
@@ -32,17 +32,16 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         jLabel7 = new javax.swing.JLabel();
         BotonInicio = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        BotonItems = new javax.swing.JPanel();
-        jLabel5 = new javax.swing.JLabel();
         BotonReportes = new javax.swing.JPanel();
         jLabel9 = new javax.swing.JLabel();
         BotonMensajes = new javax.swing.JPanel();
         jLabel10 = new javax.swing.JLabel();
+        BotonPrestamos = new javax.swing.JPanel();
+        jLabel11 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(224, 229, 233));
         setPreferredSize(new java.awt.Dimension(250, 680));
-        setSize(new java.awt.Dimension(250, 680));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Logo-3.png"))); // NOI18N
@@ -86,42 +85,6 @@ public class MenuLateral_IC extends javax.swing.JPanel {
 
         add(BotonInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 249, 250, 50));
 
-        BotonItems.setBackground(new java.awt.Color(159, 195, 231));
-        BotonItems.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                BotonItemsMouseClicked(evt);
-            }
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                BotonItemsMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                BotonItemsMouseExited(evt);
-            }
-        });
-
-        jLabel5.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Items");
-
-        javax.swing.GroupLayout BotonItemsLayout = new javax.swing.GroupLayout(BotonItems);
-        BotonItems.setLayout(BotonItemsLayout);
-        BotonItemsLayout.setHorizontalGroup(
-            BotonItemsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonItemsLayout.createSequentialGroup()
-                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 244, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-        BotonItemsLayout.setVerticalGroup(
-            BotonItemsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BotonItemsLayout.createSequentialGroup()
-                .addGap(15, 15, 15)
-                .addComponent(jLabel5)
-                .addContainerGap(14, Short.MAX_VALUE))
-        );
-
-        add(BotonItems, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 290, 250, 50));
-
         BotonReportes.setBackground(new java.awt.Color(159, 195, 231));
         BotonReportes.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -157,7 +120,7 @@ public class MenuLateral_IC extends javax.swing.JPanel {
                 .addGap(15, 15, 15))
         );
 
-        add(BotonReportes, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 340, 250, 45));
+        add(BotonReportes, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 290, 250, 45));
 
         BotonMensajes.setBackground(new java.awt.Color(159, 195, 231));
         BotonMensajes.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -194,7 +157,49 @@ public class MenuLateral_IC extends javax.swing.JPanel {
                 .addGap(15, 15, 15))
         );
 
-        add(BotonMensajes, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 380, 250, -1));
+        add(BotonMensajes, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 330, 250, -1));
+
+        BotonPrestamos.setBackground(new java.awt.Color(159, 195, 231));
+        BotonPrestamos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BotonPrestamosMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                BotonPrestamosMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                BotonPrestamosMouseExited(evt);
+            }
+        });
+
+        jLabel11.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel11.setText("Prestamos");
+        jLabel11.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel11MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout BotonPrestamosLayout = new javax.swing.GroupLayout(BotonPrestamos);
+        BotonPrestamos.setLayout(BotonPrestamosLayout);
+        BotonPrestamosLayout.setHorizontalGroup(
+            BotonPrestamosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(BotonPrestamosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 238, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        BotonPrestamosLayout.setVerticalGroup(
+            BotonPrestamosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, BotonPrestamosLayout.createSequentialGroup()
+                .addContainerGap(9, Short.MAX_VALUE)
+                .addComponent(jLabel11)
+                .addGap(15, 15, 15))
+        );
+
+        add(BotonPrestamos, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 370, 250, -1));
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
@@ -215,7 +220,7 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         if(ventanacontenedora != null){
             if(ventanacontenedora instanceof InicioAdmin){
                 System.out.print("El panel esta en inicioadmin");
-            }else if(ventanacontenedora instanceof Items || ventanacontenedora instanceof Mensajes  || ventanacontenedora instanceof Reportes){
+            }else if(ventanacontenedora instanceof Prestamos || ventanacontenedora instanceof Mensajes  || ventanacontenedora instanceof Reportes){
                 System.out.println("El panel esta en: "+ ventanacontenedora);
                 ventanacontenedora.dispose();
                 InicioAdmin iniadmin = new InicioAdmin();
@@ -238,31 +243,6 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         BotonInicio.setBackground(nuevoColor);
     }//GEN-LAST:event_BotonInicioMouseExited
 
-    private void BotonItemsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonItemsMouseClicked
-        // TODO add your handling code here:
-        JFrame ventanacontenedora = (JFrame) SwingUtilities.getWindowAncestor(this);
-
-        if(ventanacontenedora != null){
-            if(ventanacontenedora instanceof Items){
-                System.out.print("El panel esta en inicioadmin");
-            }else if(ventanacontenedora instanceof InicioAdmin || ventanacontenedora instanceof Mensajes  || ventanacontenedora instanceof Reportes){
-                System.out.println("El panel esta en: "+ ventanacontenedora);
-                ventanacontenedora.dispose();
-                InicioAdmin iniadmin = new InicioAdmin();
-                iniadmin.setVisible(true);
-
-            }
-        }
-    }//GEN-LAST:event_BotonItemsMouseClicked
-
-    private void BotonItemsMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonItemsMouseEntered
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotonItemsMouseEntered
-
-    private void BotonItemsMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonItemsMouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_BotonItemsMouseExited
-
     private void BotonReportesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonReportesMouseClicked
         // TODO add your handling code here:
 
@@ -271,7 +251,7 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         if(ventanacontenedora != null){
             if(ventanacontenedora instanceof Reportes){
                 System.out.print("El panel esta en: "+ventanacontenedora);
-            }else if(ventanacontenedora instanceof Items || ventanacontenedora instanceof InicioAdmin  || ventanacontenedora instanceof Mensajes){
+            }else if(ventanacontenedora instanceof Prestamos || ventanacontenedora instanceof InicioAdmin  || ventanacontenedora instanceof Mensajes){
                 //Se cierra la ventana en la que se esta para que se visualice la otra
                 System.out.println("El panel esta en: "+ ventanacontenedora);
                 ventanacontenedora.dispose();
@@ -303,7 +283,7 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         if(ventanacontenedora != null){
             if(ventanacontenedora instanceof Mensajes){
                 System.out.print("El panel esta en Mensajes");
-            }else if(ventanacontenedora instanceof Items   || ventanacontenedora instanceof Reportes || ventanacontenedora instanceof InicioAdmin){
+            }else if(ventanacontenedora instanceof Prestamos   || ventanacontenedora instanceof Reportes || ventanacontenedora instanceof InicioAdmin){
                 System.out.println("El panel esta en: "+ ventanacontenedora);
                 ventanacontenedora.dispose();
                 Mensajes mensajes = new Mensajes();
@@ -325,21 +305,50 @@ public class MenuLateral_IC extends javax.swing.JPanel {
         BotonMensajes.setBackground(nuevoColor);
     }//GEN-LAST:event_BotonMensajesMouseExited
 
+    private void BotonPrestamosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonPrestamosMouseClicked
+                // TODO add your handling code here:
+        JFrame ventanacontenedora = (JFrame) SwingUtilities.getWindowAncestor(this);
+
+        if(ventanacontenedora != null){
+            if(ventanacontenedora instanceof Prestamos){
+                System.out.print("El panel esta en Mensajes");
+            }else if(ventanacontenedora instanceof Mensajes   || ventanacontenedora instanceof Reportes || ventanacontenedora instanceof InicioAdmin){
+                System.out.println("El panel esta en: "+ ventanacontenedora);
+                ventanacontenedora.dispose();
+                Prestamos prestamos = new Prestamos();
+                prestamos.setVisible(true);
+
+            }
+        }
+    }//GEN-LAST:event_BotonPrestamosMouseClicked
+
+    private void BotonPrestamosMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonPrestamosMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonPrestamosMouseEntered
+
+    private void BotonPrestamosMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotonPrestamosMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotonPrestamosMouseExited
+
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         System.exit(0);
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jLabel11MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel11MouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jLabel11MouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BotonInicio;
-    private javax.swing.JPanel BotonItems;
     private javax.swing.JPanel BotonMensajes;
+    private javax.swing.JPanel BotonPrestamos;
     private javax.swing.JPanel BotonReportes;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel9;
     // End of variables declaration//GEN-END:variables
